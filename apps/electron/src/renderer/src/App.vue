1ca4157a96bf046b73deb239053dc5d219f10409
Nico Franke <nico.franke01@gmail.com>
2023-01-29 21:36:02 +0100

    Electron: Add route transition animation

--- apps/electron/src/renderer/src/App.vue
+++ apps/electron/src/renderer/src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <main class="h-screen w-screen text-white">
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" />
+      </transition>
+    </RouterView>
   </main>
 </template>
 
 <style lang="scss">
 @import './assets/css/styles.scss';
+
+.fade-leave-active {
+  transition: opacity 0.3s ease-out;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  z-index: 999;
+}
 </style>
