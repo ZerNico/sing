08abfeb99b589e3c949d1c17a1e658d9ee1b8614
ZerNico <nico.franke01@gmail.com>
2023-06-28 11:52:15 +0200

    initial project setup

---
+++ packages/eslint-config-custom/index.cjs
@@ -0,0 +1,28 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', '@tanstack/query', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'turbo',
+    '@unocss',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+  rules: {
+    'no-undef': 'off',
+    'import/no-unresolved': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+}
