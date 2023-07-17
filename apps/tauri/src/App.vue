0fc21df80cdf7625cc4da083dfdd35a77ab5a9f6
ZerNico <nico.franke01@gmail.com>
2023-07-17 14:07:26 +0200

    feat: add page transition

--- apps/tauri/src/App.vue
+++ apps/tauri/src/App.vue
@@ -2,6 +2,21 @@
 
 <template>
   <main class="font-primary font-primary h-screen w-screen text-1cqw text-white">
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" />
+      </transition>
+    </RouterView>
   </main>
 </template>
+
+<style scoped>
+.fade-leave-active {
+  transition: opacity 0.3s ease-out;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  z-index: 999;
+}
+</style>
