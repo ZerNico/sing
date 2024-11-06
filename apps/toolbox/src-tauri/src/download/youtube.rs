782afaaea7512d9c346974f3100adfe2ae1f0a67
ZerNico <nico.franke01@gmail.com>
2024-11-06 17:16:53 +0100

    fix: manually set output to allow handling the errors

--- apps/toolbox/src-tauri/src/download/youtube.rs
+++ apps/toolbox/src-tauri/src/download/youtube.rs
@@ -3,21 +3,25 @@ use std::path::Path;
 use youtube_dl::YoutubeDl;
 
 pub async fn download_youtube(url: &str, path: &Path, song_name: &str) -> Result<(), AppError> {
+    println!("Downloading youtube video");
+    println!("{:?}", path);
+
     // Download video
     YoutubeDl::new(url)
-        .format("bestvideo[height<=1080][vcodec^=avc1][vcodec^=avc]")
-        .output_template(format!("{}.mp4", song_name))
-        .download_to_async(path)
+        .format("bestvideo[height<=1080][vcodec^=avc]")
+        .extra_arg("-o")
+        .extra_arg(&format!("{}/%(title)s.%(ext)s", path.to_string_lossy()))
+        .run_async()
         .await?;
 
-    // Download and extract audio
     YoutubeDl::new(url)
         .format("bestaudio")
         .extra_arg("--extract-audio")
         .extra_arg("--audio-format")
         .extra_arg("mp3")
-        .output_template(format!("{}.mp3", song_name))
-        .download_to_async(path)
+        .extra_arg("-o")
+        .extra_arg(&format!("{}/%(title)s.%(ext)s", path.to_string_lossy()))
+        .run_async()
         .await?;
 
     Ok(())
