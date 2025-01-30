d34d5564423aac764be15c30a6114f954865f1e7
ZerNico <nico.franke01@gmail.com>
2025-01-30 18:50:51 +0100

    feat: update email template design

--- packages/email/emails/verify-email.tsx
+++ packages/email/emails/verify-email.tsx
@@ -2,36 +2,7 @@ import { Body, Container, Head, Heading, Html, Preview, Tailwind, type TailwindC
 
 const tailwindConfig: TailwindConfig = {
   theme: {
-    extend: {
-      colors: {
-        night: {
-          "50": "#f0f4fd",
-          "100": "#e3ebfc",
-          "200": "#ccd8f9",
-          "300": "#adbdf4",
-          "400": "#8c9bed",
-          "500": "#7079e4",
-          "600": "#5556d6",
-          "700": "#4645bd",
-          "800": "#3b3c98",
-          "900": "#363879",
-          "950": "#101024",
-        },
-        spearmint: {
-          "50": "#effef3",
-          "100": "#d8ffe6",
-          "200": "#b4fecf",
-          "300": "#7afbab",
-          "400": "#38ef7d",
-          "500": "#0fd85b",
-          "600": "#06b348",
-          "700": "#098c3c",
-          "800": "#0d6e33",
-          "900": "#0d5a2d",
-          "950": "#003316",
-        },
-      },
-    },
+    extend: {},
   },
 };
 
@@ -39,7 +10,7 @@ export interface VerifyEmailProps {
   code: string;
   url: string;
   supportUrl: string;
-};
+}
 
 export function VerifyEmail(props: VerifyEmailProps) {
   const currentYear = new Date().getFullYear();
@@ -49,32 +20,42 @@ export function VerifyEmail(props: VerifyEmailProps) {
       <Head />
       <Preview>Verify your E-Mail</Preview>
       <Tailwind config={tailwindConfig}>
-        <Body className="mx-auto my-auto bg-night-950 px-2 py-10 font-sans text-slate-50">
+        <Body className="mx-auto my-auto bg-[#203141] px-2 py-10 font-sans text-slate-800">
           <Container className="mx-auto max-w-[465px] py-5 text-center">
-            <Heading className="m-0 font-semibold text-[40px]">Tune Perfect</Heading>
+            <Heading className="m-0 font-semibold text-[30px] text-white">Tune Perfect</Heading>
           </Container>
-          <Container className="mx-auto max-w-[465px] rounded border border-white/10 border-solid p-[20px]">
+          <Container className="mx-auto w-full max-w-[465px] rounded-lg bg-white p-[32px]">
             <Heading className="m-0 font-semibold text-[20px]">Verify your E-Mail</Heading>
-            <Text className="m-0 text-slate-300">Enter the following code to verify your email address.</Text>
-            <Text className="m-0 p-8 text-center ">
-              <span className="inline-block rounded border border-white/10 border-solid p-4 font-bold text-[40px] tracking-[6px]">{props.code}</span>
+            <Text className="m-0 text-slate-500">Enter the following code to verify your email address.</Text>
+            <Text className="m-0 py-12 text-center ">
+              <span className="inline-block rounded border border-white/10 border-solid font-bold text-[40px] tracking-[6px]">
+                {props.code}
+              </span>
             </Text>
 
             <Text className="m-0 pb-8 text-center">
-              <a href={props.url} target="_blank" className="inline-block rounded bg-slate-50 px-4 py-2 font-semibold text-[14px] text-black no-underline" rel="noreferrer">
+              <a
+                href={props.url}
+                target="_blank"
+                className="box-border inline-block w-full rounded-lg px-4 py-2 font-semibold text-[14px] text-white no-underline shadow-md"
+                rel="noreferrer"
+                style={{
+                  background: "linear-gradient(to right, #36d1dc, #5b86e5)",
+                }}
+              >
                 Visit Tune Perfect
               </a>
             </Text>
-            <Text className="m-0 text-slate-300">If you didn't request this, you can safely ignore this email.</Text>
+            <Text className="m-0">If you didn't request this, you can just ignore this email.</Text>
           </Container>
-          <Container className="mx-auto max-w-[465px] py-5 text-center">
-            <Text className="m-0 text-slate-300">
+          <Container className="mx-auto max-w-[465px] py-5 text-center text-white">
+            <Text className="m-0">
               Need help?{" "}
-              <a href={props.supportUrl} target="_blank" className="text-spearmint-500 no-underline" rel="noreferrer">
+              <a href={props.supportUrl} target="_blank" className="text-white no-underline" rel="noreferrer">
                 Contact Support
               </a>
             </Text>
-            <Text className="m-0 text-slate-300">© {currentYear} Tune Perfect.</Text>
+            <Text className="m-0">© {currentYear} Tune Perfect.</Text>
           </Container>
         </Body>
       </Tailwind>
