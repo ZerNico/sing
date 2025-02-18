28c82a3a28ae98ac204e13219d3e29af3b631d64
ZerNico <nico.franke01@gmail.com>
2025-02-18 12:48:47 +0100

    feat: add language switcher

--- apps/app/src/components/header.tsx
+++ apps/app/src/components/header.tsx
@@ -3,10 +3,13 @@ import { useQueryClient } from "@tanstack/solid-query";
 import { Show } from "solid-js";
 import { v1 } from "~/lib/api";
 import { useAuth } from "~/lib/auth";
-import { t } from "~/lib/i18n";
+import { setLocale, t } from "~/lib/i18n";
 import { profileQueryOptions } from "~/lib/queries";
 import { notify } from "~/lib/toast";
+import IconDe from "~icons/circle-flags/de";
+import IconEnUs from "~icons/circle-flags/en-us";
 import IconBan from "~icons/lucide/ban";
+import IconEarth from "~icons/lucide/earth";
 import IconLogOut from "~icons/lucide/log-out";
 import IconUser from "~icons/lucide/user";
 import NavItems from "./nav-items";
@@ -37,14 +40,16 @@ export default function Header() {
     <>
       <div class="h-16" />
       <header class="fixed top-0 right-0 left-0 border-white/10 border-b">
-        <div class="mx-auto flex h-16 max-w-6xl items-center justify-between gap-2 px-4">
+        <div class="mx-auto grid h-16 max-w-6xl grid-cols-[1fr_auto_1fr] items-center justify-between gap-2 px-4">
           <div>
             <span class="font-bold text-lg">{t("header.app_name")}</span>
           </div>
           <div class="flex flex-grow justify-center">
-            <NavItems class="hidden md:flex" />
+            <Show when={profile()}>
+              <NavItems class="hidden md:flex" />
+            </Show>
           </div>
-          <div>
+          <div class="flex justify-end gap-2">
             <Show when={profile()}>
               {(profile) => (
                 <DropdownMenu>
@@ -68,6 +73,19 @@ export default function Header() {
                 </DropdownMenu>
               )}
             </Show>
+            <DropdownMenu>
+              <DropdownMenu.Trigger class="cursor-pointer rounded-full p-2 hover:bg-white/10 focus-visible:outline-2 focus-visible:outline-white">
+                <IconEarth />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Item onClick={() => setLocale("en")}>
+                  <IconEnUs /> English
+                </DropdownMenu.Item>
+                <DropdownMenu.Item onClick={() => setLocale("de")}>
+                  <IconDe /> Deutsch
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu>
           </div>
         </div>
       </header>
